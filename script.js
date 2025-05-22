0f5f6f4ccd0abb1089d8cb796b6da807cb257d9b
Coi <ctoyco1@up.edu.ph>
2025-05-22 18:41:28 +0800

    Added Image

--- script.js
+++ script.js
@@ -2,13 +2,42 @@ function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
   
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+console.log("hello world");
+
+function openForm() {
+    document.getElementById("signup").style.display = "block";
 }
 
-function Login(event){
-  event.preventDefault();
+const signUpButton=document.getElementById('signUpButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const signUpForm=document.getElementById('signup');
+
+function showNav() {
   document.getElementById("nav").style.display = "flex";
-  document.getElementById("SignUp").style.display = "none";
-  closeForm();
+}
+
+signUpButton.addEventListener('click',function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+    console.log("1234");
+})
+
+signInButton.addEventListener('click', function(){
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+    console.log("abcd");
+})
+
+
+function increaseQuantity() {
+  const qty = document.getElementById("quantity");
+  qty.value = parseInt(qty.value) + 1;
+}
+
+function decreaseQuantity() {
+  const qty = document.getElementById("quantity");
+  if (parseInt(qty.value) > 1) {
+    qty.value = parseInt(qty.value) - 1;
+  }
 }
