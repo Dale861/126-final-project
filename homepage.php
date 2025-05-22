1b5d3db8b2f02c9d1369e7ce9a2300bb84e1c402
Coi <ctoyco1@up.edu.ph>
2025-05-22 19:01:33 +0800

    fixed style href

--- homepage.php
+++ homepage.php
@@ -12,7 +12,7 @@ include("connect.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register & Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="signUp-style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <main>
